8bae5237f1b327657a1e9fe9ed5e8a41e4338218
1667 <1157215781@qq.com>
2016-06-18 20:35:50 +0800

    增加AVPlayer操作

---
+++ TTTT/AVFoundationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVFoundationViewController.h
+//  TTTT
+//
+//  Created by innke on 16/6/18.
+//  Copyright © 2016年 wuxianying. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AVFoundationViewController : BaseViewController
+
+@end
